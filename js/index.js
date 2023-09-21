855f7e65c02138bd101313aff8e10013a1067ded
bestbrain06 <136890174+bestbrain06@users.noreply.github.com>
2023-09-21 05:38:42 +0000

    final commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-const numbers = document.querySelectorAll(".number");
+let submit = document.querySelector("#submit");
+let rateAgain = document.querySelector("#rate-again");
+let card1 = document.querySelector(".card__1");
+let card2 = document.querySelector(".card__2");
+let rating = document.querySelectorAll(".rating");
+let rates = document.querySelector(".rates");
 
-const form = document.querySelector("form");
+submit.addEventListener("click", () => {
+  card2.classList.remove("hidden");
+  card1.classList.add("hidden");
+});
+
+rateAgain.addEventListener("click", () => {
+  card2.classList.add("hidden");
+  card1.classList.remove("hidden");
+});
+
+rating.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rates.textContent = rate.textContent;
+  });
+});
